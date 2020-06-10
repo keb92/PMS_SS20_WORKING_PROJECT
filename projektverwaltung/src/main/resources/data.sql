629152d53116280c9c0cc68c8745ab0ba53f7b47
Kevin Bauer <kevin.bauer@stud.th-deg.de>
2020-06-10 11:28:19 +0200

    data.sql fix

--- projektverwaltung/src/main/resources/data.sql
+++ projektverwaltung/src/main/resources/data.sql
@@ -4,7 +4,7 @@ REPLACE INTO `roles` VALUES (1,'ADMIN'), (2,'MEMBER');
 
 -- Testdaten - 2 Teamleiter - 3 Mitarbeiter
 REPLACE INTO `users` VALUES (1,1, "Heike.Neustadt@thd-projektverwaltung.de", "Teamleiter","Neustadt", 6, "Heike", "$2a$10$dRk99Fs/pKlMVCKHREknFO14J2tdCbqT4sGA.PdnYR9yOAHNPBnSO", "hneustadt", 100),
-                            (2,1, "Stephanie.Braun@projektverwaltung.de", "Projektleiter","Braun", 6, "Stephanie", "$2a$10$dRk99Fs/pKlMVCKHREknFO14J2tdCbqT4sGA.PdnYR9yOAHNPBnSO", "sbraun", 100);
+                            (2,1, "Stephanie.Braun@projektverwaltung.de", "Projektleiter","Braun", 6, "Stephanie", "$2a$10$dRk99Fs/pKlMVCKHREknFO14J2tdCbqT4sGA.PdnYR9yOAHNPBnSO", "sbraun", 100),
                             (3,1, "Benjamin.Zweig@projektverwaltung.de", "Entwickler","Benjamin", 6, "Zweig", "$2a$10$dRk99Fs/pKlMVCKHREknFO14J2tdCbqT4sGA.PdnYR9yOAHNPBnSO", "bzweig", 100),
                             (4,1, "Marko.Junker@projektverwaltung.de", "SAP-Berater","Junker", 6, "Marko", "$2a$10$dRk99Fs/pKlMVCKHREknFO14J2tdCbqT4sGA.PdnYR9yOAHNPBnSO", "mjunker", 100),
                             (5,1, "Dirk.Decker@projektverwaltung.de", "Solution Architekt","Decker", 6, "Dirk", "$2a$10$dRk99Fs/pKlMVCKHREknFO14J2tdCbqT4sGA.PdnYR9yOAHNPBnSO", "ddecker", 100);
