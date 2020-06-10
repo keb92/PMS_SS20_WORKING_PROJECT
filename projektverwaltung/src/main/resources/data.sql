a18e58e6cc6d8b04256fb515bc8230fe4711409b
Kevin Bauer <kevin.bauer@stud.th-deg.de>
2020-06-10 10:34:06 +0200

    SQL-Datenbasis User

--- projektverwaltung/src/main/resources/data.sql
+++ projektverwaltung/src/main/resources/data.sql
@@ -1,2 +1,7 @@
+
 REPLACE INTO `roles` VALUES (1,'ADMIN'), (2,'MEMBER');
+
+
+REPLACE INTO `users` VALUES (1,1, "Heike.Neustadt@thd-projektverwaltung.de", "Teamleiter","Neustadt", 6, "Heike", "$2a$10$dRk99Fs/pKlMVCKHREknFO14J2tdCbqT4sGA.PdnYR9yOAHNPBnSO", "hneustadt", 100),
+                            (2,1, "Stephanie.Braun@projektverwaltung.de", "Projektleiter","Braun", 6, "Stephanie", "$2a$10$dRk99Fs/pKlMVCKHREknFO14J2tdCbqT4sGA.PdnYR9yOAHNPBnSO", "sbraun", 100);
 
